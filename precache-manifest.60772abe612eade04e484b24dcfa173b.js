233cf3aac057ba0d597cc06fcc6bb1ae6a1fd896
Jander Marques <jander@softimais.com.br>
2019-09-25 17:53:36 -0300

    Updates

---
+++ precache-manifest.60772abe612eade04e484b24dcfa173b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729ae0216f10c26e59a5f66d2792bdd1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3fa3c16d2bc499af6c2f",
+    "url": "/monsters-rolodex/static/css/main.a3ac8313.chunk.css"
+  },
+  {
+    "revision": "d13a353a0fe4879286ec",
+    "url": "/monsters-rolodex/static/js/2.d395d0d5.chunk.js"
+  },
+  {
+    "revision": "3fa3c16d2bc499af6c2f",
+    "url": "/monsters-rolodex/static/js/main.d7f0d39c.chunk.js"
+  },
+  {
+    "revision": "f9d70e3af54ddae19106",
+    "url": "/monsters-rolodex/static/js/runtime-main.e20afc0a.js"
+  }
+]);
